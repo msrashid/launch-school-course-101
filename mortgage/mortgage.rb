78aa1212130a0b277903dd2dc0b6dfbe1f8c1df4
Mohammed Rashid <msrashid1995@gmail.com>
2016-07-15 22:53:38 +0000

    final adjustment

--- mortgage/mortgage.rb
+++ mortgage/mortgage.rb
@@ -1,21 +1,21 @@
 require 'yaml'
-MG = YAML.load_file('mortgage.yml')
+MESSAGE = YAML.load_file('mortgage.yml')
 
 amount = "To Be Determined"
 duration = "To Be Determined"
 rate = "To Be Determined"
 
 # print format
-def read(x)
-  read = MG["en"][x]
+def prompt(prompt_name)
+  read = MESSAGE["en"][prompt_name]
   puts "=> #{read}"
 end
 
 # valid number (0 and above)
-def valid_number?(x)
-  if !(x =~ /[a-zA-Z]/) && x =~ /0/ && x.to_f == 0
+def valid_number?(number)
+  if !(number =~ /[a-zA-Z]/) && number =~ /0/ && number.to_f == 0
     false
-  elsif x.to_f >= 1 && !(x =~ /[a-zA-Z]/)
+  elsif number.to_f >= 1 && !(number =~ /[a-zA-Z]/)
     true
   else
     false
@@ -23,28 +23,28 @@ def valid_number?(x)
 end
 
 # welcome
-read("welcome")
+prompt("welcome")
 
 # amount
 loop do
-  read("amount")
+  prompt("amount")
   amount = gets.chomp.delete("$")
-  break if valid_number?(amount) ? true : read("valid")
+  break if valid_number?(amount) ? true : prompt("valid")
 end
 
 # duration
 loop do
-  read("duration")
+  prompt("duration")
   duration = gets.chomp
-  break if valid_number?(duration) ? true : read("valid")
+  break if valid_number?(duration) ? true : prompt("valid")
 end
 
 # rate
 loop do
-  read("rate")
+  prompt("rate")
   rate = gets.chomp
   valid_rate = valid_number?(rate) && rate.to_f < 100
-  break if valid_rate ? true : read("valid")
+  break if valid_rate ? true : prompt("valid")
 end
 
 amount = amount.to_f
@@ -54,7 +54,7 @@ adjusted_rate = (rate.to_f / 1200)
 # monthly payment
 payment = ((amount * (adjusted_rate * ((1 + adjusted_rate)**duration))) /
                   ((1 + adjusted_rate)**duration - 1))
-read("payment")
+prompt("payment")
 puts "$#{payment.round(2)}"
 
 # goodbye
