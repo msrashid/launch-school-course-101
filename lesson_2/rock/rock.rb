f21bf6e1c2c7e836fbdcb4981e11fc40ff26a6d8
Mohammed Rashid <msrashid1995@gmail.com>
2016-07-17 22:30:31 +0000

    final changes

--- lesson_2/rock/rock.rb
+++ lesson_2/rock/rock.rb
@@ -1,9 +1,9 @@
 require 'yaml'
 MESSAGE = YAML.load_file('rock.yml')
-option = "TO BE DETERMINED"
-player = 0
-computer = 0
-options = {
+option = nil
+player_score = 0
+computer_score = 0
+OPTIONS = {
   "r" => "rock",
   "p" => "paper",
   "s" => "scissors",
@@ -12,64 +12,66 @@ options = {
 }
 
 # prompt
-loop do
   def prompt(prompt_name)
     puts "=> #{prompt_name}"
   end
 
-  # win or lose logic
-  def win_logic?(user_input, computer_input)
-    win = %w(p r sp r s l s p l sp r s l p sp)
-    true_false = false
-    5.times do |x|
-      true_false = (user_input == win[3 * x] &&
-      (computer_input == win[3 * x + 1] || computer_input == win[3 * x + 2]))
-      break if true_false
-    end
-    true_false
-  end
-
+# win or lose logic
+def did_player_win?(user_input, computer_input)
+  # keys represents user_input, arrays represent computer_input
+  winning_combinations_for_player = { 'p' => %w(r sp), 
+                                      'r' => %w(s l),
+                                      's' => %w(p l),
+                                      'sp' => %w(r s),
+                                      'l' => %w(l sp) }
+  winning_combinations_for_player[user_input].include?(computer_input)
+end
+  
+loop do
   # determining inputs for user and computer
-  computer_option = options.keys.sample
+  computer_option = OPTIONS.keys.sample
   loop do
     puts MESSAGE['Welcome']
     option = gets.chomp.downcase
-    options.include?(option) ? break : prompt("Try again:")
+    OPTIONS.include?(option) ? break : prompt("Try again:")
   end
-  prompt("Player chose: #{options[option]}")
-  prompt("Computer chose: #{options[computer_option]}")
+  prompt("Player chose: #{OPTIONS[option]}")
+  prompt("Computer chose: #{OPTIONS[computer_option]}")
 
   # computing round victor and displaying scoreboard
   if option == computer_option
     prompt("It's a tie!")
     next
-  elsif win_logic?(option, computer_option)
+  elsif did_player_win?(option, computer_option)
     prompt("One point for Player!")
+    player_score += 1
   else
     prompt("One point for Computer!")
+    computer_score += 1
   end
   puts
-  win_logic?(option, computer_option) ? player += 1 : computer += 1
   score = <<-SCOREBOARD
 
           Scoreboard
         Player    |   Wins
         ----------------------
-        Computer  |  #{computer}
-        You       |  #{player}
+        Computer  |  #{computer_score}
+        You       |  #{player_score}
 
   SCOREBOARD
+  puts score
 
   # Determining winner and loser
-  if player == 5
+  if player_score == 5
     prompt("Player Won!")
     puts score
-  elsif computer == 5
+    break
+  elsif computer_score == 5
     prompt("Computer Won!")
     puts score
+    break
   end
-  break if player == 5 || computer == 5
-  puts score
+
 end
 
 # goodbye!
