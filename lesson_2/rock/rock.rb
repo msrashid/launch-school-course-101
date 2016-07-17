89612f482919db3af53fda4a09ed7b3df2dd541f
Mohammed Rashid <msrashid1995@gmail.com>
2016-07-17 22:43:40 +0000

    Fixed everything

--- lesson_2/rock/rock.rb
+++ lesson_2/rock/rock.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 MESSAGE = YAML.load_file('rock.yml')
-option = nil
+player_choice = nil
 player_score = 0
 computer_score = 0
 OPTIONS = {
@@ -12,37 +12,37 @@ OPTIONS = {
 }
 
 # prompt
-  def prompt(prompt_name)
-    puts "=> #{prompt_name}"
-  end
+def prompt(prompt_name)
+  puts "=> #{prompt_name}"
+end
 
-# win or lose logic
+# did the player win? if not he lost
 def did_player_win?(user_input, computer_input)
   # keys represents user_input, arrays represent computer_input
-  winning_combinations_for_player = { 'p' => %w(r sp), 
+  winning_combinations_for_player = { 'p' => %w(r sp),
                                       'r' => %w(s l),
                                       's' => %w(p l),
                                       'sp' => %w(r s),
                                       'l' => %w(l sp) }
   winning_combinations_for_player[user_input].include?(computer_input)
 end
-  
+
 loop do
   # determining inputs for user and computer
-  computer_option = OPTIONS.keys.sample
+  computer_choice = OPTIONS.keys.sample
   loop do
     puts MESSAGE['Welcome']
-    option = gets.chomp.downcase
-    OPTIONS.include?(option) ? break : prompt("Try again:")
+    player_choice = gets.chomp.downcase
+    OPTIONS.include?(player_choice) ? break : prompt("Try again:")
   end
-  prompt("Player chose: #{OPTIONS[option]}")
-  prompt("Computer chose: #{OPTIONS[computer_option]}")
+  prompt("Player chose: #{OPTIONS[player_choice]}")
+  prompt("Computer chose: #{OPTIONS[computer_choice]}")
 
-  # computing round victor and displaying scoreboard
-  if option == computer_option
+  # computing round victor, displaying scoreboard, checking to see if tied
+  if player_choice == computer_choice
     prompt("It's a tie!")
     next
-  elsif did_player_win?(option, computer_option)
+  elsif did_player_win?(player_choice, computer_choice)
     prompt("One point for Player!")
     player_score += 1
   else
@@ -71,7 +71,6 @@ loop do
     puts score
     break
   end
-
 end
 
 # goodbye!
